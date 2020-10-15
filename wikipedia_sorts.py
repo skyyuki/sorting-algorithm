9e333916e07c2196f812a23ee775ef3b52927055
skyyuki <skyyuki6@outlook.jp>
2020-10-15 14:24:14 +0900

    feat: 🎸 generate_target

--- wikipedia_sorts.py
+++ wikipedia_sorts.py
@@ -1,4 +1,29 @@
 import argparse
+import random
+from typing import List
+
+
+def generate_target(sequence: str, size: int) -> List[int]:
+    target_ = list(range(1, size + 1))
+    if sequence == 'random':
+        random.shuffle(target_)
+    elif sequence == 'sorted':
+        pass
+    elif sequence == 'reversed':
+        target_ = list(reversed(target_))
+    elif sequence == 'almost-sorted':
+        target_.append(target_.pop(size // 2))
+    elif sequence == 'sorted-roughly':
+        # TODO: use shell_sort or comb_sort
+        pass
+    elif sequence == 'few-unique':
+        target_ = [(1 + i // (size // 4)) * (size // 4) for i in range(size)]
+        random.shuffle(target_)
+    return target_
+
+
+def draw_charts(algorithms: List[str], interval: int):
+    pass
 
 
 if __name__ == '__main__':
@@ -16,5 +41,6 @@ if __name__ == '__main__':
     group.add_argument('--interval', type=int)
     group.add_argument('--fps', type=int)
     args = parser.parse_args()
+    target = generate_target(args.target, args.size)
 
     print(args)
