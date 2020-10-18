0ed3de5959beddbc8aa4233415a35228589e0f5d
skyyuki <skyyuki6@outlook.jp>
2020-10-19 03:15:27 +0900

    feat: 🎸 implement ChartElement #3
    ChartElement create and implement some comparison method

---
+++ utils.py
@@ -0,0 +1,35 @@
+
+class ChartElement:
+    def __init__(self, v: int):
+        self._v = v
+
+    def __repr__(self):
+        return self._v
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self._v == other._v
+        else:
+            kls = other.__class__.__name__
+            raise NotImplementedError(
+                f'comparison between ChartElement and {kls} is not supported')
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __lt__(self, other):
+        if isinstance(other, self.__class__):
+            return self._v < other._v
+        else:
+            kls = other.__class__.__name__
+            raise NotImplementedError(
+                f'comparison between ChartElement and {kls} is not supported')
+
+    def __le__(self, other):
+        return self.__lt__(other) or self.__eq__(other)
+
+    def __gt__(self, other):
+        return not self.__le__(other)
+
+    def __ge__(self, other):
+        return not self.__lt__(other)
