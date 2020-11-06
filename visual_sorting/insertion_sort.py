fed70a8d01f27073196ff067bb007abb4289fca7
skyyuki <skyyuki6@outlook.jp>
2020-11-06 20:53:35 +0900

    fix: 🐛 about the creation of frame objects.
    Fixed frames elements show the same object, and fixed time subtraction.
    +1L *6L

--- visual_sorting/insertion_sort.py
+++ visual_sorting/insertion_sort.py
@@ -1,3 +1,4 @@
+import copy
 from time import perf_counter as time
 from typing import Tuple, List, Dict, Union
 
@@ -11,10 +12,10 @@ def insertion_sort(target: List[ChartElement]) -> List[Tuple[list, Dict[str, Uni
             'comparisons': 0,
             'reads': 0,
             'write': 0}
-    frames = [(target, info)]
+    frames = [(target, info.copy())]
     for i in range(len(a)):
-        info['time'] = start_time - time()
-        frames.append((a, info))
+        info['time'] = time() - start_time
+        frames.append((copy.deepcopy(a), info.copy()))
         frames[-1][0][i].color = 'r'
         for j in range(i, 0, -1):
             info['comparisons'] += 1
@@ -22,10 +23,10 @@ def insertion_sort(target: List[ChartElement]) -> List[Tuple[list, Dict[str, Uni
             if a[j] < a[j-1]:
                 a[j-1], a[j] = a[j], a[j-1]
                 info['write'] += 2
-                info['time'] = start_time - time()
-                frames.append((a, info))
+                info['time'] = time() - start_time
+                frames.append((copy.deepcopy(a), info.copy()))
             else:
                 break
-    info['time'] = start_time - time()
+    info['time'] = time() - start_time
     frames.append((a, info))
     return frames
